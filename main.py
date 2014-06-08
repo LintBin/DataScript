86557467b9e9878e4e5908181491ae677fe62a30
LintBin <312763889@qq.com>
2014-06-08 23:41:42 +0800

    try

--- main.py
+++ main.py
@@ -3,45 +3,20 @@ import os,os.path
 import string,base64
 import string
 def main():
-    table=input("请输入数据表名:")
-    li=[]
-    li=check(li)
-    f=open("inserData.sql",'w')
-    i=1
-    num=input("将要生成的sql语句多少条？:")
-    numStr=str(num)
-    while i<int(num):
-        sql=createsql(table,li,str(i))
-        i=i+1
-        print(sql)
-        
-    
-    #sql="insert into values "+table+"("+li[0]+"','"
-    #print(sql)
- 
-    f.close()
+    print("hello world")
+    check()
 
-def inputColumn():
-    column=input("请输入列名:")
-    return column
-    
-def check(li):
-    flag=input("是否还有列要输入，是则输入'y'，没有则输入'n':")
+def check():
+    print("hello check")
+    flag=input("是否还要添加字段(是则输入'y',否则输入其他字母):")
     if flag=="y":
-        li.append(inputColumn())
-        check(li)
+        numStr=input("生成次类型的字段的个数:")
+        
     if flag=="n":
-        return li
-    else:
-        return li
-def createsql(table,li,numStr):
-    sql="insert into "+table+"("
-    for element in li:
-        sql=sql+"'"+element+numStr+"',"
-    sql=sql[:-1]
-    sql=sql+")"
-    return sql
+        print("this is orther")
 
-
+def column():
+    print("请输入列名")
+    print("sadsad")
 if __name__=='__main__':  
     main()
